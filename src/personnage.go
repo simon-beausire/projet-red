5a35ba14d56316a0ec5bbb9257b863c92f301833
simon-beausire <simon.beausire@ynov.com>
2025-09-08 11:38:29 +0200

    update personnage

--- src/personnage.go
+++ src/personnage.go
@@ -1,1 +1,15 @@
 package main
+
+type Inventaire struct {
+	NomObjet string
+	Quantite int
+}
+
+type User struct {
+	Nom              string
+	Classe           string
+	Niveau           string
+	PdvMax           int
+	PdvActuel        int
+	InventaireJoueur []Inventaire
+}
