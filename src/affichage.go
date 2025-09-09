941b3417fb2ac693bf66991b24cff8a4b7fcda64
simon-beausire <simon.beausire@ynov.com>
2025-09-09 09:42:04 +0200

    Update affichage.go

--- src/affichage.go
+++ src/affichage.go
@@ -11,15 +11,47 @@ func Espace(esp int, chaine1 string, chaine2 string) string {
 }
 
 func (u User) Affichage() {
-	tab := [][]string{
+	TabStat := [][]string{
 		{"------------------------------"},
 		{Espace(29, "Nom Joueur", u.Nom)},
 		{Espace(29, "Classe", u.Classe)},
 		{Espace(29, "Niveau", string(u.Niveau+48))},
 		{Espace(29, "Points de vie ", string(u.PdvActuel+48))},
 		{"------------------------------"},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                              "},
+		{"                             "},
 	}
-	for i := 0; i < len(tab); i++ {
-		fmt.Println(tab[i])
+	for i := 0; i < 36; i++ {
+		for index := range TabStat[i] {
+			fmt.Print("|", TabStat[i][index], "|", "------------------------------------------------------------------------------------------------------")
+		}
 	}
 }
