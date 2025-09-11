7fbcfa4cedca9af910c2622e4ab075b0fb622157
benj880 <benjamin.serrure@ynov.com>
2025-09-11 09:11:54 +0200

    Update objects.go

--- src/objects.go
+++ src/objects.go
@@ -1,1 +1,11 @@
 package main
+
+func (u *User) poposoin() {
+	valeur := "potion_de_soin"
+	for i, v := range u.InventaireJoueur {
+		if v.NomObjet == valeur {
+			u.ajoutervie(3)
+			u.InventaireJoueur = append(u.InventaireJoueur[:i], u.InventaireJoueur[i+1:]...)
+		}
+	}
+}
